3772b1ee8599e132de4874cec1167052e2ebd0b9
Jose Antonio Sousa <josepromo31@gmail.com>
2023-08-03 10:11:41 +0200

    1er commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,59 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto, LoginUserDto, RegisterUserDto, UpdateAuthDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
+import { User } from './entities/user.entity';
+import { LoginResponse } from './interfaces/login-response';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  create(@Body() createUser: CreateUserDto) {
+    return this.authService.create(createUser);
+  }
+
+  @Post('/login')
+  login( @Body() loginDto: LoginUserDto) {
+    return this.authService.login (loginDto);
+  }
+
+  @Post('/register')
+  register( @Body() registerDto: RegisterUserDto) {
+    return this.authService.register (registerDto);
+  }
+
+  @UseGuards (AuthGuard)
+  @Get()
+  findAll( @Request() req: Request) {
+    return this.authService.findAll();
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('check-token')
+  checkToken( @Request() req: Request): LoginResponse {
+   
+    const user = req['user'] as User;
+
+    return {
+      user,
+      token: this.authService.getJwt({ id: user._id })
+    }
+  }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.authService.remove(+id);
+  // }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,109 @@
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';import {  } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs';
+import { LoginUserDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload';
+import { LoginResponse } from './interfaces/login-response';
+import { CreateUserDto, UpdateAuthDto, RegisterUserDto } from './dto';
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    @InjectModel ( User.name ) 
+      private userModel: Model<User>,
+      private jwtService: JwtService,
+  ) {}
+  
+  
+  //Grabar en BBDD
+  async create (createUserDto: CreateUserDto): Promise<User> {
+
+    try {
+
+      //Encriptar contraseña
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync( password, 10 ),
+        ...userData
+      });
+
+      await newUser.save();
+
+      //Quitar contraseña del objeto para que no se vea en la petición
+      const { password:_, ...user } = newUser.toJSON();
+
+      return user;
+
+    } catch (error) {
+      if (error.code === 11000)
+        throw new BadRequestException(`${ createUserDto.email } alredy exists`);
+    }
+    throw new InternalServerErrorException('Error');
+
+  }
+
+  async register ( registerDto: RegisterUserDto): Promise<LoginResponse> {
+
+    const user = await this.create (registerDto);
+
+    return {
+      user: user,
+      token: this.getJwt({ id: user._id })
+    }
+  }
+
+  async login (loginDto: LoginUserDto): Promise<LoginResponse> {
+
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email: email });
+    if ( !user ) {
+      throw new UnauthorizedException('Not valid credentials - email');
+    }
+
+    if ( !bcryptjs.compareSync (password, user.password)){
+      throw new UnauthorizedException('Not valid credentials - password');  
+    }
+
+    const { password:_, ...rest } = user.toJSON();
+
+    return {
+      user: rest,
+      token: this.getJwt ({ id:user.id }),
+    }
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userModel.find();
+  }
+
+  async findUserById (id: string) {
+    const user = await this.userModel.findById(id);
+    const { password, ...rest } = user.toJSON();
+    return rest;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: UpdateAuthDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+
+  getJwt ( payload: JwtPayload) {
+    
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
+}
